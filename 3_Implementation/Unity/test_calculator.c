ada0d1566d10cc199fd424c3d0f13d75be47bbd1
Ankarvaishnavi <vaishnaviankar2@gmail.com>
2022-02-11 14:07:14 +0530

    code updation

--- 3_Implementation/Unity/test_calculator.c
+++ 3_Implementation/Unity/test_calculator.c
@@ -1,3 +1,26 @@
 #include "unity.h"
 #include "operation.h"
+void addition();
+void test_add(void);
+
+void setUp (){
+
+}
+void tearDown()
+{
+    
+}
+int main(){
+    UNITY_BEGIN();
+    RUN_TEST(test_add);
+    return UNIY_END();
+    }
+void test_add(void){
+    int number1, number2;
+    int compute=number1+number2;
+    TEST_ASSERT_EQUAL(number1+number2, compute);
+
+}    
+
+
 
